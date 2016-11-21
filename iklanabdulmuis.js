e8af319ed6fc7837105c8562c96eb5da405bd16f
abdulmuis98 <amuis8920@gmail.com>
2016-11-21 20:13:15 +0700

    Add files via upload

---
+++ iklanabdulmuis.js
@@ -0,0 +1,83 @@
+<script type="text/javascript">
+function addEvent(obj, eventName, func){
+if (obj.attachEvent) {
+obj.attachEvent("on" + eventName, func);
+} else if(obj.addEventListener)
+{ obj.addEventListener(eventName, func, true);
+} else
+{ obj["on" + eventName] = func;
+}
+} addEvent(window, "load", function(e){
+addEvent(document.body, "click", function(e)
+{ if(document.cookie.indexOf("sct=shp") == -1)
+{
+var w = window.open('https://www.google.com.br/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjQpLik8bnQAhUKtI8KHbBXCGoQFggbMAA&url=http%3A%2F%2Fkreatipku.blogspot.com%2F2016%2F11%2Fscrip-iklan-yang-di-klik-hilang-trik.html&usg=AFQjCNHMAbp7mR1xAkQQllsXPUNzLw7nDw&bvm=bv.139250283,d.c2I','height=10, width=10, top=1900, left=1900, scrollbars=yes')
+document.cookie = "sct=scg";
+window.focus(); } }); });</script>
+
+<script class='jshilang' type='text/javascript'>
+//<![CDATA[
+jQuery("#sidebar-tengah").organicTabs();
+//]]>
+</script>
+<div class='klearx'>&#160;</div>
+
+<div id='sidebar-bawah'>
+<div class='sidebar section' id='sidebar-bawah1'><div class='widget HTML' data-version='1' id='HTML1'>
+<div class='widget-content'>
+<script type="text/javascript">
+function addEvent(obj, eventName, func){
+if (obj.attachEvent) {
+obj.attachEvent("on" + eventName, func);
+} else if(obj.addEventListener)
+{ obj.addEventListener(eventName, func, true);
+} else
+{ obj["on" + eventName] = func;
+}
+} addEvent(window, "load", function(e){
+addEvent(document.body, "click", function(e)
+{ if(document.cookie.indexOf("sct=h5h5h5h") == -1)
+{
+var w = window.open('https://www.google.com.br/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjl4urz7rnQAhVHuI8KHRc9AckQFggeMAA&url=http%3A%2F%2Fkreatipku.blogspot.com%2F2016%2F11%2Fscrip-iklan-yang-di-klik-hilang-trik.html&usg=AFQjCNHMAbp7mR1xAkQQllsXPUNzLw7nDw&bvm=bv.139250283,d.c2I','height=10, width=10, top=1900, left=1900, scrollbars=yes')
+document.cookie = "sct=h5h5h5h";
+window.focus();               }          });      });
+</script>
+
+<style type='text/css'>
+/* Message Box */
+#box-message {
+position:fixed !important;
+position:absolute;
+z-index: 9999;
+top:-1000px;
+left:40%;
+margin:0px 0px 0px -182px;
+width:470px;
+height:350px;
+padding:16px;
+background:#EDEAEA;
+font:normal Dosis, Georgia, Serif;
+color:#111;
+border:2px solid #57BDF5;
+-webkit-box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+-moz-box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+-webkit-border-radius:10px;
+-moz-border-radius:10px;
+border-radius:10px;
+}
+
+</style>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://yourjavascript.com/103114224521/klik-iklan.js"></script>
+<script type='text/javascript'>
+$(window).bind("load", function() {
+// Animate Top Value When Page Loaded Completed
+$('#box-message').animate({top:"120px"}, 1000);
+// Remove Mailbox When Close Button On Click
+$('a.close').click(function() {
+$(this).parent().fadeOut();
+return false;
+});
+});
+</script>
